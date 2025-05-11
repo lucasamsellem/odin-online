851c8449c9d4dce45119268f131333e4d25acde1
Lucas Amsellem <lucas.amsellem21@gmail.com>
2025-05-11 15:35:54 +0200

    feat: minor CSS improvements

--- src/layout/Board.tsx
+++ src/layout/Board.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useRef } from 'react';
-import Card from '../components/Card';
-import { CARD_COLORS, NUMBER_OF_PLAYERS } from '../data/gameRules';
-import { buildCardNumber } from '../utils/buildCardNumber';
+import { useEffect, useRef } from 'react'
+import Card from '../components/Card'
+import { CARD_COLORS, NUMBER_OF_PLAYERS } from '../data/gameRules'
+import { buildCardNumber } from '../utils/buildCardNumber'
 import {
   CardsOnBoard,
   CurrentTurn,
@@ -12,19 +12,19 @@ import {
   OnPlayerWarning,
   OnWasBoardEmpty,
   WasBoardEmpty,
-} from '../types/GameProps';
+} from '../types/GameProps'
 
 type BoardProps = {
-  cardsOnBoard: CardsOnBoard;
-  currentTurn: CurrentTurn;
-  onCurrentTurn: OnCurrentTurn;
-  onCardsOnBoard: OnCardsOnBoard;
-  wasBoardEmpty: WasBoardEmpty;
-  onWasBoardEmpty: OnWasBoardEmpty;
-  latestPlayedCards: LatestPlayedCards;
-  onPlayedCardsThisTurn: OnPlayedCardsThisTurn;
-  onPlayerWarning: OnPlayerWarning;
-};
+  cardsOnBoard: CardsOnBoard
+  currentTurn: CurrentTurn
+  onCurrentTurn: OnCurrentTurn
+  onCardsOnBoard: OnCardsOnBoard
+  wasBoardEmpty: WasBoardEmpty
+  onWasBoardEmpty: OnWasBoardEmpty
+  latestPlayedCards: LatestPlayedCards
+  onPlayedCardsThisTurn: OnPlayedCardsThisTurn
+  onPlayerWarning: OnPlayerWarning
+}
 
 function Board({
   cardsOnBoard,
@@ -37,51 +37,51 @@ function Board({
   onPlayedCardsThisTurn,
   onPlayerWarning,
 }: BoardProps) {
-  const isBoardEmpty = cardsOnBoard.length === 0;
+  const isBoardEmpty = cardsOnBoard.length === 0
 
   const latestRoundNumbers = Array.isArray(latestPlayedCards)
-    ? latestPlayedCards?.map((card) => card.number)
-    : cardsOnBoard[0]?.number;
+    ? latestPlayedCards?.map(card => card.number)
+    : cardsOnBoard[0]?.number
 
-  const prevRoundNb = useRef<number | null>(null);
+  const prevRoundNb = useRef<number | null>(null)
 
   useEffect(() => {
-    prevRoundNb.current = latestRoundNumbers;
-  }, [latestRoundNumbers]);
+    prevRoundNb.current = latestRoundNumbers
+  }, [latestRoundNumbers])
 
   const triggerNextTurn = () => {
     if (isBoardEmpty) {
-      return onPlayerWarning('You must play a card first turn.');
+      return onPlayerWarning('You must play a card first turn.')
     }
 
     // Reset played cards count to distinct between turns
-    onPlayedCardsThisTurn(0);
+    onPlayedCardsThisTurn(0)
 
-    const nextTurn = currentTurn >= NUMBER_OF_PLAYERS ? 1 : currentTurn + 1;
-    onCurrentTurn(nextTurn);
-    onWasBoardEmpty(false);
-  };
+    const nextTurn = currentTurn >= NUMBER_OF_PLAYERS ? 1 : currentTurn + 1
+    onCurrentTurn(nextTurn)
+    onWasBoardEmpty(false)
+  }
 
   const resetBoard = () => {
-    onCardsOnBoard([]);
-    onWasBoardEmpty(false);
-  };
+    onCardsOnBoard([])
+    onWasBoardEmpty(false)
+  }
 
   return (
-    <div className='board'>
+    <div className="board">
       <header>
-        <button onClick={resetBoard} className='reset-board-btn'>
+        <button onClick={resetBoard} className="reset-board-btn">
           Reset board
         </button>
         <h2>Board</h2>
 
-        <span className='lastest-played-cards-number'>
+        <span className="lastest-played-cards-number">
           {buildCardNumber(latestRoundNumbers)} 🔥
         </span>
       </header>
 
-      <ul className='cards-list'>
-        {Object.values(cardsOnBoard).map((card) => (
+      <ul className="cards-list">
+        {Object.values(cardsOnBoard).map(card => (
           <li key={card.id}>
             <Card card={card} />
           </li>
@@ -90,7 +90,7 @@ function Board({
 
       <button
         onClick={triggerNextTurn}
-        className='next-turn-btn'
+        className="next-turn-btn"
         style={{
           backgroundColor: CARD_COLORS[wasBoardEmpty ? 'red' : 'blue'],
         }}
@@ -98,7 +98,7 @@ function Board({
         &#8594; Next turn
       </button>
     </div>
-  );
+  )
 }
 
-export default Board;
+export default Board
